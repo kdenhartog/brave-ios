2479863cee81de3521acb4196fb0e37b999551e6
Kyle Hickinson <git@kylehickinson.com>
2022-03-16 12:12:30 -0400

    Integrate JS bindings with new provider APIs

--- Client/Frontend/Browser/UserScriptManager.swift
+++ Client/Frontend/Browser/UserScriptManager.swift
@@ -127,7 +127,8 @@ class UserScriptManager {
     isPaymentRequestEnabled: Bool,
     isWebCompatibilityMediaSourceAPIEnabled: Bool,
     isMediaBackgroundPlaybackEnabled: Bool,
-    isNightModeEnabled: Bool
+    isNightModeEnabled: Bool,
+    walletProviderJS: String?
   ) {
     self.tab = tab
     self.isFingerprintingProtectionEnabled = isFingerprintingProtectionEnabled
@@ -137,7 +138,7 @@ class UserScriptManager {
     self.isPlaylistEnabled = true
     self.isMediaBackgroundPlaybackEnabled = isMediaBackgroundPlaybackEnabled
     self.isNightModeEnabled = isNightModeEnabled
-
+    self.walletProviderJS = walletProviderJS
     reloadUserScripts()
   }
 
@@ -401,6 +402,8 @@ class UserScriptManager {
                         in: .page)
   }()
 
+    private var walletProviderJS: String?
+    
   private func reloadUserScripts() {
     tab?.webView?.configuration.userContentController.do {
       $0.removeAllUserScripts()
@@ -451,6 +454,9 @@ class UserScriptManager {
             
             if let script = walletProviderScript {
                 $0.addUserScript(script)
+                if let providerJS = walletProviderJS {
+                    $0.addUserScript(.init(source: providerJS, injectionTime: .atDocumentStart, forMainFrameOnly: true, in: .page))
+                }
             }
     }
   }
